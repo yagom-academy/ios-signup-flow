0ac868579440cde16f472c50d0ce6360226e5596
yeon <jryoun1@naver.com>
2020-11-30 21:19:35 +0900

    feat: UserInformation 타입 생성

--- SignUpFlow/SignUpFlow/UserInformation.swift
+++ SignUpFlow/SignUpFlow/UserInformation.swift
@@ -5,4 +5,13 @@
 //  Created by Yeon on 2020/11/30.
 //
 
-import Foundation
+class UserInformation {
+    static let shared: UserInformation = UserInformation()
+    private init() {}
+    var userID: String?
+    var userPassWord: String?
+    var userImage: String?
+    var userIntroduction: String?
+    var userPhone: String?
+    var userBirthDate: String?
+}
