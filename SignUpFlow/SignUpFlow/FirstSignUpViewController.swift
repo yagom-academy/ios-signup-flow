841fff5738a2fa499332bf454993d47eb65a7a46
zziro95 <opers7878@gmail.com>
2020-12-01 12:34:07 +0900

    feat: 스토리보드 구성 및 뷰컨트롤러 생성

---
+++ SignUpFlow/SignUpFlow/FirstSignUpViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FirstSignUpViewController.swift
+//  SignUpFlow
+//
+//  Created by Jinho Choi on 2020/12/01.
+//
+
+import UIKit
+
+class FirstSignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ SignUpFlow/SignUpFlow/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  SignUpFlow
+//
+//  Created by Jinho Choi on 2020/12/01.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func touchUpSignUpButton(_ sender: Any) {
+    }
+}
