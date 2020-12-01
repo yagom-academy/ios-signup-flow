c58bccb6ed768182da5681a44de0d20920febf01
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-12-01 16:53:19 +0900

    Feat : 각 화면 기능 구현에 필요한 프로퍼티 추가

--- SignUpFlow/SignUpFlow/UserInformation.swift
+++ SignUpFlow/SignUpFlow/UserInformation.swift
@@ -5,12 +5,24 @@
 //  Created by Kyungmin Lee on 2020/12/01.
 //
 
-import Foundation
+import UIKit
 
-class UserInformation {
+final class UserInformation {
     // 싱글턴 인스턴스
     static let shared: UserInformation = UserInformation()
     
+    // 로그인 화면에 필요한 프로퍼티
+    var id: String?
+    var password: String?
+    
+    // 과정 1 화면에 추가로 필요한 프로퍼티
+    var description: String?
+    var profileImage: UIImage?
+    
+    // 과정 2 화면에 필요한 프로퍼티
+    var phoneNumber: String?
+    var birthdate: String?
+
     // private로 선언해서 다른곳에서 객체생성이 불가능하게 한다
     private init() {}
 }
