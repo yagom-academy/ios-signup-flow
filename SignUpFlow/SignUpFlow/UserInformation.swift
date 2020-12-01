0140e8e78dfb2e75b517be148753b775eccef0f5
GREENOVER <humains29@gmail.com>
2020-12-01 11:09:28 +0900

    refactor(Step1 / 그린,찌루루): 로직에 따른 순서 및 변수, 타입 변경, 메서드 제거

--- SignUpFlow/SignUpFlow/UserInformation.swift
+++ SignUpFlow/SignUpFlow/UserInformation.swift
@@ -1,27 +1,16 @@
 import Foundation
+import UIKit
 
 class UserInformation {
-    var id: String?
-    var password: String?
-    var phoneNumber: String?
-    var dateOfBirth: Date?
-    var profileImage: String?
-    var introduction: String?
-    
     static let shared = UserInformation()
     
     private init() {
     }
-
-    func isSame(password: String, checkPassword: String) -> Bool {
-        return password == checkPassword
-    }
     
-    func filterCharacter(phoneNumber: String) -> String {
-        if let _ = Int(phoneNumber) {
-            return phoneNumber
-        } else {
-            return phoneNumber.filter { $0.isNumber }
-        }
-    }
+    var id: String?
+    var password: String?
+    var phoneNumber: String?
+    var birthDate: Date?
+    var profileImage: UIImage?
+    var introduction: String?
 }
