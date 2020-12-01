820e3245fa3f3fa9b099b4a3c849b1063bed51d2
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-12-02 00:48:39 +0900

    Feat : LoginViewController로 클래스명 변경, 화면 구성요소 배치

---
+++ SignUpFlow/SignUpFlow/ViewControllers/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpFlow - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom academy. All rights reserved.
+// 
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+}
+
