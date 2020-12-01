84805a80b00022c4cf523fec60f7a63ec0984355
GREENOVER <humains29@gmail.com>
2020-12-01 12:20:37 +0900

    feat(Step2 / 그린,찌루루): 텍스트 필드 외 영역 터치 시 키보드 숨김 기능 추가

--- SignUpFlow/SignUpFlow/ViewController.swift
+++ SignUpFlow/SignUpFlow/ViewController.swift
@@ -5,7 +5,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+         self.view.endEditing(true)
+   }
 }
 
