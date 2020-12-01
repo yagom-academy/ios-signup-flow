1c21e1fb2055b7696e71ebc29da6becabc2403bf
odong-Tree <ss12357@naver.com>
2020-12-01 14:51:35 +0900

    feat: @IBOutlet, @IBAction 추가

--- SignUpFlow/SignUpFlow/ViewController.swift
+++ SignUpFlow/SignUpFlow/ViewController.swift
@@ -2,8 +2,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appLogoImageView: UIImageView!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    @IBAction func touchUpSignUpButton(_ sender: UIButton) {
     }
 }
