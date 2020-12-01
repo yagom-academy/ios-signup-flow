7481e9de5b74b89ebfb8c30159e458bd63facc1a
odong-Tree <ss12357@naver.com>
2020-12-01 16:15:25 +0900

    style: UserInformation 프로퍼티명 변경

--- SignUpFlow/SignUpFlow/ViewController.swift
+++ SignUpFlow/SignUpFlow/ViewController.swift
@@ -14,3 +14,10 @@ class ViewController: UIViewController {
     @IBAction func touchUpSignUpButton(_ sender: UIButton) {
     }
 }
+
+
+//1. 뷰에 아이템 다 올리기
+//2. 키보드
+//3. 화면 이동 구현 / 다음 화면 뷰 생성
+//    * 모달? 네비게이션?
+//                    네이게이션, 절차를 밟은 거니까?
