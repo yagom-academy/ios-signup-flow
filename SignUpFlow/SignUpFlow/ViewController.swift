ae3a5e84a24aa5fcf8ede1dd0daef427a002cc75
odong-Tree <ss12357@naver.com>
2020-12-01 17:18:31 +0900

    feat: 화면 터치시 키보드 내리는 메서드 추가

--- SignUpFlow/SignUpFlow/ViewController.swift
+++ SignUpFlow/SignUpFlow/ViewController.swift
@@ -15,9 +15,19 @@ class ViewController: UIViewController {
     }
 }
 
+// 키보드 제어
+extension ViewController {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+         self.view.endEditing(true)
+   }
+}
 
-//1. 뷰에 아이템 다 올리기
-//2. 키보드
-//3. 화면 이동 구현 / 다음 화면 뷰 생성
-//    * 모달? 네비게이션?
-//                    네이게이션, 절차를 밟은 거니까?
+/*
+1. 뷰에 아이템 다 올리기
+2. 키보드
+ 키보드 커스터마이징
+ 엔터 눌렀을 때 키보드 내리기
+3. 화면 이동 구현 / 다음 화면 뷰 생성
+    * 모달? 네비게이션?
+    네이게이션, 절차를 밟은 거니까?
+*/
