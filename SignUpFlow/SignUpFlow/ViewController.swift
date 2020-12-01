5d609e9659bc0da07b57ba7deaab043fef8e01b3
wonhee <wonhee010@gmail.com>
2020-12-01 17:04:04 +0900

    feat: add hide keyboard

--- SignUpFlow/SignUpFlow/ViewController.swift
+++ SignUpFlow/SignUpFlow/ViewController.swift
@@ -15,5 +15,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
