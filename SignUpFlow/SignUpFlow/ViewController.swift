11c344799245dcc7f0e67c1795511316f3d15561
wonhee <wonhee010@gmail.com>
2020-12-01 16:42:45 +0900

    feat: add IBOutlet

--- SignUpFlow/SignUpFlow/ViewController.swift
+++ SignUpFlow/SignUpFlow/ViewController.swift
@@ -7,12 +7,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var IDField: UITextField!
+    @IBOutlet weak var Password: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
