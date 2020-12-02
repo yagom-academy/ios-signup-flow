6cc00d88bd18590f41984bd4e0885cb37053915c
zdodev <iver0945@gmail.com>
2020-12-02 10:09:26 +0900

    feat: UIImagePickerController 생성

--- SignUpFlow/SignUpFlow/SignUpStep1ViewController.swift
+++ SignUpFlow/SignUpFlow/SignUpStep1ViewController.swift
@@ -1,13 +1,21 @@
 import UIKit
+import MobileCoreServices
 
 class SignUpStep1ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
+    let imagePicker: UIImagePickerController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 
     @IBAction func pressedImageView(_ sender: UITapGestureRecognizer) {
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.mediaTypes = [kUTTypeImage as String]
         
+        present(imagePicker, animated: true)
     }
 }
