e48f6df1098444636310e5e38b8619272c4be7a1
zziro95 <opers7878@gmail.com>
2020-12-02 10:31:43 +0900

    feat: IBOutlet연결 & nextButton 비활성화

--- SignUpFlow/SignUpFlow/FirstSignUpViewController.swift
+++ SignUpFlow/SignUpFlow/FirstSignUpViewController.swift
@@ -8,8 +8,18 @@
 import UIKit
 
 class FirstSignUpViewController: UIViewController {
-
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var newIdTextField: UITextField!
+    @IBOutlet weak var newPasswordTextField: UITextField!
+    @IBOutlet weak var checkPasswordTextField: UITextField!
+    @IBOutlet weak var introductionTextView: UITextView!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.nextButton.isEnabled = false
     }
 }
