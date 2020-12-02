9e1904d52413046192edc2bc59778ea270057928
yeon <jryoun1@naver.com>
2020-12-02 14:25:03 +0900

    feat: tapCance() 구현(cancel(), userInformationReset() 호출)

--- SignUpFlow/SignUpFlow/SignUpViewController.swift
+++ SignUpFlow/SignUpFlow/SignUpViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
-
+class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordCheckTextField: UITextField!
+    @IBOutlet weak var introductionTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.isNavigationBarHidden = true
+    }
+    
+    @IBAction private func tapCancelButton() {
+        userInformationReset()
+        cancel()
+    }
+    
+    private func userInformationReset() {
+        UserInformation.shared.id = nil
+        UserInformation.shared.password = nil
+        UserInformation.shared.image = nil
+        UserInformation.shared.introduction = nil
+        UserInformation.shared.phone = nil
+        UserInformation.shared.birthDate = nil
+    }
+
+    private func cancel() {
+        self.dismiss(animated: true, completion: nil)
     }
 }
