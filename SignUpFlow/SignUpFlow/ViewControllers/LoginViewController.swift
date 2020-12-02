cfda8dffcda475de72a679f066cd0e076d019dd3
iluxsm <be9.smile@gmail.com>
2020-12-02 18:55:35 +0900

    Feat : TextField 관련 기능 추가
    각 TextField의 ReturnKeyType, ClearButtonMode, SecureTextEntry 설정

--- SignUpFlow/SignUpFlow/ViewControllers/LoginViewController.swift
+++ SignUpFlow/SignUpFlow/ViewControllers/LoginViewController.swift
@@ -11,7 +11,35 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // ID
+        idTextField.returnKeyType = .next
+        idTextField.clearButtonMode = .whileEditing
+        //Password
+        passwordTextField.returnKeyType = .done
+        passwordTextField.clearButtonMode = .whileEditing
+        passwordTextField.isSecureTextEntry = true
+    }
     
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 }
 
+//MARK:- Extensions
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case idTextField:
+            idTextField.resignFirstResponder()
+            passwordTextField.becomeFirstResponder()
+        case passwordTextField:
+            passwordTextField.resignFirstResponder()
+        default:
+            break
+        }
+        return true
+    }
+}
