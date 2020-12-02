8df9b52ff33d479e95e85b454e77169d20c4ab57
iluxsm <be9.smile@gmail.com>
2020-12-02 19:50:06 +0900

    Refactor : 불필요한 코드 제거
    viewDidLoad() 및 firstResponder 중복 제거

--- SignUpFlow/SignUpFlow/ViewControllers/LoginViewController.swift
+++ SignUpFlow/SignUpFlow/ViewControllers/LoginViewController.swift
@@ -11,10 +11,6 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
     }
@@ -25,7 +21,6 @@ extension LoginViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         switch textField {
         case idTextField:
-            idTextField.resignFirstResponder()
             passwordTextField.becomeFirstResponder()
         case passwordTextField:
             passwordTextField.resignFirstResponder()
