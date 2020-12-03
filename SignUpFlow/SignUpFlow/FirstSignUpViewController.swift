63b787b9493550a26a8fb15b55f280b710a7f6c1
innieminnie <rkddlsl1001@gmail.com>
2020-12-04 02:33:58 +0900

    feat: 프로필 이미지 설정 기능 구현

--- SignUpFlow/SignUpFlow/FirstSignUpViewController.swift
+++ SignUpFlow/SignUpFlow/FirstSignUpViewController.swift
@@ -21,5 +21,32 @@ class FirstSignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.nextButton.isEnabled = false
+        
+        let profileImageTapGesture = UITapGestureRecognizer.init(target: self, action: #selector(pickProfileImage))
+        profileImageView.addGestureRecognizer(profileImageTapGesture)
+        profileImageView.isUserInteractionEnabled = true
+    }
+    
+}
+extension FirstSignUpViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @objc func pickProfileImage() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.allowsEditing = true
+        imagePickerController.delegate = self
+        self.present(imagePickerController,animated: true,completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage = info[.editedImage] as? UIImage {
+            self.profileImageView.image = selectedImage
+            user.profileImage = selectedImage
+        } else if let selectedImage = info[.originalImage] as? UIImage {
+            self.profileImageView.image = selectedImage
+            user.profileImage = selectedImage
+        }
+
+        self.dismiss(animated: true, completion: nil)
     }
 }
