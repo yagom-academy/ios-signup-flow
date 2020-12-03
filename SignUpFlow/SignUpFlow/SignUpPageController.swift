9aecc81458679c9d7885f34e87f2358e7b24c330
odong-Tree <ss12357@naver.com>
2020-12-03 22:08:13 +0900

    feat: imageView, Button IB 코드 추가 및 연결

--- SignUpFlow/SignUpFlow/SignUpPageController.swift
+++ SignUpFlow/SignUpFlow/SignUpPageController.swift
@@ -2,7 +2,19 @@ import UIKit
 
 class SignUpPageController: UIViewController {
     
+    @IBOutlet weak var userImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func selectImageButton(_ sender: UIButton) {
+    }
+}
+
+extension SignUpPageController: UITextFieldDelegate {
+    // 화면 터치시 키보드 내리기
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+         self.view.endEditing(true)
+   }
 }
