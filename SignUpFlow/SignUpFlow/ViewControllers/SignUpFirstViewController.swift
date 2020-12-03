29276bebc4410cd17e1840f64aacbe60c828bf03
iluxsm <be9.smile@gmail.com>
2020-12-03 17:07:50 +0900

    Feat : 프로필사진 라이브러리를 통해 가져오도록 구현

--- SignUpFlow/SignUpFlow/ViewControllers/SignUpFirstViewController.swift
+++ SignUpFlow/SignUpFlow/ViewControllers/SignUpFirstViewController.swift
@@ -9,5 +9,36 @@ import UIKit
 
 class SignUpFirstViewController: UIViewController {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    private var imagePickerController = UIImagePickerController()
+    
+    @objc private func setProfileImage() {
+        imagePickerController.sourceType = UIImagePickerController.SourceType.savedPhotosAlbum
+        imagePickerController.allowsEditing = true
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePickerController.delegate = self
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(setProfileImage))
+        profileImage.addGestureRecognizer(tapGesture)
+        profileImage.isUserInteractionEnabled = true
+    }
 
+}
+
+//MARK:- Extensions
+extension SignUpFirstViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            profileImage.image = selectedImage
+            // 원본 비율 유지하면서 꽉 차게하는 임시 코드. 이건 아까 스토리보드에서 설정하지 않아서 나중에 옮겨 놓을거예요!
+            profileImage.contentMode = .scaleAspectFill
+            UserInformation.card.profileImage = self.profileImage.image
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
