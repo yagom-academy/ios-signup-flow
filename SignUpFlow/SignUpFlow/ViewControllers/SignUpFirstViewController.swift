a01258df702d0e1d5b7cc4fd8e2deca689aabc00
iluxsm <be9.smile@gmail.com>
2020-12-03 18:13:43 +0900

    Fix : 프로필 사진이 라이브러리 편집 사진으로 적용되도록 수정
    -Description
    info.plist에 포토 라이브러리 관련 Privacy 추가

--- SignUpFlow/SignUpFlow/ViewControllers/SignUpFirstViewController.swift
+++ SignUpFlow/SignUpFlow/ViewControllers/SignUpFirstViewController.swift
@@ -33,7 +33,7 @@ class SignUpFirstViewController: UIViewController {
 //MARK:- Extensions
 extension SignUpFirstViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+        if let selectedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
             profileImage.image = selectedImage
             // 원본 비율 유지하면서 꽉 차게하는 임시 코드. 이건 아까 스토리보드에서 설정하지 않아서 나중에 옮겨 놓을거예요!
             profileImage.contentMode = .scaleAspectFill
