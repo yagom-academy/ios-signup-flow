a4890732403945080f653b02c68403ae3dd8b1ab
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-12-03 15:46:32 +0900

    Feat : IBOutlet 추가 - 아이디, 비밀번호, 소개, 버튼

--- SignUpFlow/SignUpFlow/ViewControllers/SignUpFirstViewController.swift
+++ SignUpFlow/SignUpFlow/ViewControllers/SignUpFirstViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class SignUpFirstViewController: UIViewController {
-
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var checkPasswordTextField: UITextField!
+    @IBOutlet weak var introductionTextView: UITextView!
+    @IBOutlet weak var nextButton: UIButton!
+    
 
 }
