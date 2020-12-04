5c3f915b721cccbeac4ffd31a898e01970d75ebf
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-12-04 21:47:26 +0900

    add : 스토리 보드에 UI 추가 및 ViewController와 연결
    스토리 보드에 UI 추가 및 ViewController와 연결

--- SignUpFlow/SignUpFlow/RegisterStepOneViewController.swift
+++ SignUpFlow/SignUpFlow/RegisterStepOneViewController.swift
@@ -9,7 +9,19 @@ import UIKit
 
 class RegisterStepOneViewController: UIViewController {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var checkPasswordTextField: UITextField!
+    @IBOutlet weak var introduceTextView: UITextView!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func didTapNextButton(_ sender: UIButton) {
+    }
+    @IBAction func didTapCancelButton(_ sender: UIButton) {
     }
 }
