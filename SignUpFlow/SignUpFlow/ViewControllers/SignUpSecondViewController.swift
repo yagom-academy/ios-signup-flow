f11b30b41f8355980b34beb6bec8cdae4929e31e
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-12-04 12:13:32 +0900

    Feat : IBOutlet 추가

--- SignUpFlow/SignUpFlow/ViewControllers/SignUpSecondViewController.swift
+++ SignUpFlow/SignUpFlow/ViewControllers/SignUpSecondViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class SignUpSecondViewController: UIViewController {
+    // MARK: - IBOutlets
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var birthDateLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var signUpButton: UIButton!
+    
     // MARK: - Properites
     class var storyboardID: String {
         return "\(self)"
