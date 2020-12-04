adb1d0ee2919b06e98496a83989d019acf7381dc
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-12-04 22:10:13 +0900

    feat : profile image view를 탭할 시 photo library로 이동
    profile image view를 탭하는 것을 인지하는 UITapGestureRecognizer를 만들고, UIImagePicker를 활용하여 이미지를 선택할 수 있게 함.

--- SignUpFlow/SignUpFlow/RegisterStepOneViewController.swift
+++ SignUpFlow/SignUpFlow/RegisterStepOneViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class RegisterStepOneViewController: UIViewController {
-
+    
     //MARK: - IBOutlets
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var idTextField: UITextField!
@@ -20,11 +20,36 @@ class RegisterStepOneViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        nextButton.isEnabled = false
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapProfileImageView(_:)))
+        profileImageView.isUserInteractionEnabled = true
+        profileImageView.addGestureRecognizer(tap)
     }
     
     //MARK: - IBActions
     @IBAction func didTapNextButton(_ sender: UIButton) {
     }
     @IBAction func didTapCancelButton(_ sender: UIButton) {
+    }
+}
+
+extension RegisterStepOneViewController {
+    @objc func didTapProfileImageView(_ sender: UITapGestureRecognizer) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
+
+extension RegisterStepOneViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let pickedImage = info[.editedImage] as? UIImage else {
+            fatalError("No Image Picked!!")
+        }
+        self.profileImageView.image = pickedImage
     }
 }
