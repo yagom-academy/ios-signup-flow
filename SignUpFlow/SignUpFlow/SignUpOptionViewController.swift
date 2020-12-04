ed89c84a926f6d000517bbee14b559f69130e69c
yeon <jryoun1@naver.com>
2020-12-04 15:51:26 +0900

    feat: SignUpOptionViewController에서의 UIOutlet 프로퍼티 생성, SignUpViewController의 객체를 받을 userInfoTemporarySave 프로퍼티 생성

--- SignUpFlow/SignUpFlow/SignUpOptionViewController.swift
+++ SignUpFlow/SignUpFlow/SignUpOptionViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class SignUpOptionViewController: UIViewController {
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var birthDateDisplayLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    var userInfoTemporarySave: UserInfoTemporarySave = UserInfoTemporarySave()
     
     @IBAction func goBack() {
         self.dismiss(animated: true, completion: nil)
