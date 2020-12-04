9b556bedefdc73078d954907c63581fadfa030c2
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-12-05 00:23:47 +0900

    feat : 모든 Field가 비어있는지에 따라 Next Button 활성화
    NotificationCenter와 프로퍼티 감시자를 활용하여 필드가 비어있는지 비어있지 않은지 확인하고자 하였고, 비어있다면 NextButton 비활성화, 다 채워져있다면 활성화 하게 구현.

--- SignUpFlow/SignUpFlow/RegisterStepOneViewController.swift
+++ SignUpFlow/SignUpFlow/RegisterStepOneViewController.swift
@@ -18,9 +18,21 @@ class RegisterStepOneViewController: UIViewController {
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     
+    private var profileImage: UIImage? {
+        didSet {
+            profileImageView.image = profileImage
+            checkAllField()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         nextButton.isEnabled = false
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(checkAllField),
+                                               name: UITextField.textDidChangeNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(checkAllField),
+                                               name: UITextView.textDidChangeNotification, object: nil)
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(didTapProfileImageView(_:)))
         profileImageView.isUserInteractionEnabled = true
@@ -29,10 +41,22 @@ class RegisterStepOneViewController: UIViewController {
     
     //MARK: - IBActions
     @IBAction func didTapNextButton(_ sender: UIButton) {
-//        프로필 이미지, 아이디, 자기소개가 모두 채워지고,
-//        패스워드가 일치하면 '다음' 버튼이 활성화됩니다.
-        
+        guard let id = idTextField.text, let password = passwordTextField.text, let profileImage = profileImageView.image,
+              let introduce = introduceTextView.text else {
+            fatalError("빈칸이 있습니다!")
+        }
 
+        let member = MemberList.common
+        switch member.addNewMember(id: idTextField.text ?? "") {
+        case .success(_):
+            member.members[id]?.setPassword(password)
+            member.members[id]?.setProfileImage(profileImage)
+            member.members[id]?.setIntroduction(introduce)
+        case .failure(let message):
+            fatalError(message.errorDescription ?? "에러 메시지가 없습니다.")
+        }
+        NotificationCenter.default.removeObserver(UITextField.textDidChangeNotification)
+        NotificationCenter.default.removeObserver(UITextView.textDidChangeNotification)
     }
     @IBAction func didTapCancelButton(_ sender: UIButton) {
         profileImageView.image = nil
@@ -52,6 +76,37 @@ extension RegisterStepOneViewController {
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
     }
+    @objc func checkAllField() {
+        let checkTextFields = [idTextField, passwordTextField, checkPasswordTextField].filter { $0?.text == "" }
+        let isImageViewNotEmpty = profileImageView.image == nil ? false : true
+        let isTextViewNotEmpty = introduceTextView.text == "" ? false : true
+        
+        if checkTextFields.isEmpty && isImageViewNotEmpty && isTextViewNotEmpty && checkValidatePassword() {
+            nextButton.isEnabled = true
+        }
+        else {
+            nextButton.isEnabled = false
+        }
+    }
+    
+    private func checkValidatePassword() -> Bool {
+        //passwordTextField가 비어있지 않고 서로 같으면
+        if (passwordTextField.text != "") && (passwordTextField.text == checkPasswordTextField.text) {
+            return true
+        }
+        return false
+    }
+    private func configureTextField(_ textField: UITextField) {
+        if textField == idTextField {
+            textField.autocorrectionType = .no
+            textField.autocapitalizationType = .none
+            textField.keyboardType = .emailAddress
+        }
+        else if textField == passwordTextField {
+            textField.autocorrectionType = .no
+            textField.autocapitalizationType = .none
+        }
+    }
 }
 
 extension RegisterStepOneViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -60,6 +115,6 @@ extension RegisterStepOneViewController: UIImagePickerControllerDelegate, UINavi
         guard let pickedImage = info[.editedImage] as? UIImage else {
             fatalError("No Image Picked!!")
         }
-        self.profileImageView.image = pickedImage
+        self.profileImage = pickedImage
     }
 }
